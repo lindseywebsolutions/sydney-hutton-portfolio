1dc7df609027c69455ba16fcf187ff2057333e17
xlindseyj <xlindseyj@outlook.com>
2022-05-26 14:49:57 -0400

    Updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './containers/home-page/home-page.component';
 import { BodyComponent } from './components/body/body.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    BodyComponent
+    BodyComponent,
+    AboutMeComponent,
+    PortfolioComponent,
+    ResumeComponent,
+    HeaderComponent,
+    SideNavigationComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
