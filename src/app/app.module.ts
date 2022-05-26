3501688b3cbe225b2c86e0572b4b6499e7f9eaa5
xlindseyj <xlindseyj@outlook.com>
2022-05-26 14:00:22 -0400

    Project initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './containers/home-page/home-page.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
